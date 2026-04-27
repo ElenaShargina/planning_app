06afab4ceb167be9bf6e4f6fcd6558e4edec7c01
elena <elena.shargina@gmail.com>
2026-04-28 00:05:27 +0300

    fix nginx in settings

--- planning_app/settings.py
+++ planning_app/settings.py
@@ -119,3 +119,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
